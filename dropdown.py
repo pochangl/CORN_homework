65d7a761de17ced61dc2caef89749b1c4b9ba3f2
Pochang Lee <pochanglee2008@gmail.com>
2023-08-10 13:31:58 +0800

    autocommit

--- dropdown.py
+++ dropdown.py
@@ -6,7 +6,7 @@ app = Dash()
 app.layout = html.Div(children = [
     dcc.Dropdown(options=[], id='dropdown1'),
     html.Button(children='初始化按鈕', id='button', n_clicks=0),
-    html.Button(children='按鈕', id='button2', n_clicks=0),
+    html.Button(children='讀取資料', id='button2', n_clicks=0),
     html.H1(id='output'),
 ])
 
@@ -19,6 +19,14 @@ app.layout = html.Div(children = [
 def on_click(n_clicks):
     return ['物件1', '物件2', '物件3']
 
+
+@app.callback(
+    Output('output', 'children'),
+    Input('button2', 'n_clicks'),
+    State('dropdown1', 'value'),
+)
+def on_load(n_clicks, dropdown_value):
+    return f'選擇了 {dropdown_value}'
 
 
 app.run(debug=True)
