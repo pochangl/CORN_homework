b58f803a7dd4e79795889c1192fc9e752d7c4719
Pochang Lee <pochanglee2008@gmail.com>
2023-08-10 13:20:14 +0800

    autocommit

--- dropdown.py
+++ dropdown.py
@@ -4,7 +4,7 @@ from dash import Dash, Input, Output, dcc, html, callback
 app = Dash()
 
 app.layout = html.Div(children = [
-    dcc.DropDown(value=[], id='dropdown1'),
+    dcc.Dropdown(value=[], id='dropdown1'),
     html.Button(id='button', n_click=0)
 ])
 @callback(
