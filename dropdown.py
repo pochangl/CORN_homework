5aa49008a0d36d39558645fb0c7fdb0953ee303f
Pochang Lee <pochanglee2008@gmail.com>
2023-08-10 13:21:27 +0800

    autocommit

--- dropdown.py
+++ dropdown.py
@@ -7,13 +7,14 @@ app.layout = html.Div(children = [
     dcc.Dropdown(value=[], id='dropdown1'),
     html.Button(id='button', n_clicks=0)
 ])
-@callback(
+
+
+@app.callback(
     Output('doropdown1', 'value'),
     Input('button', 'n_clicks'),
 )
-
 def on_click(n_clicks):
-    return ['物件1', '物件2', '物件2']
+    return ['物件1', '物件2', '物件3']
 
 
 
